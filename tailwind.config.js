b718848805684029fbd2940f4964227804e9f167
AHMAD SAAD <dn32206@gmail.com>
2024-10-21 19:30:03 +0500

    nav-bar

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+    //   gridTemplateRows: {
+    //     // Simple 16 row grid
+    //     '3': 'repeat(3, 1fr)'
+    // },
+    height: {
+      '80vh': '80vh',
+    }
+  },
+  plugins: [],
+}}
+
